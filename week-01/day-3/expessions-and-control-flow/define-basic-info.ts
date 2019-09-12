5cdfc16f30fe91dd082e72c91b0225d827c89e5d
lakospal0 <lakospal0@gmail.com>
2019-09-12 17:34:29 +0200

    I did this exercise

---
+++ week-01/day-3/expessions-and-control-flow/define-basic-info.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myName : string = "Pál Lakos";
+let myAge : number = 20;
+let myHeight: number = 1.72;
+let myMarrige: boolean= false;
+console.log("My name is: "+ myName + "\n" + myAge + " Years old"+"\n"+ "I'm "+ myHeight+"\n"+"Am I married?-> "+ myMarrige );
