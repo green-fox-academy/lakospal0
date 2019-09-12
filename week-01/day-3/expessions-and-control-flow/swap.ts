a3e63eca6dd6fc81280dcb6bce16e0781ffc71d4
lakospal0 <lakospal0@gmail.com>
2019-09-12 17:06:27 +0200

    I don't think that I did this in proper way

---
+++ week-01/day-3/expessions-and-control-flow/swap.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+
+console.log("This is variable a: " + a);
+console.log("This is variable b: " + b);
+console.log("this is new a :" + b);
+console.log("This is new b: " + a);
