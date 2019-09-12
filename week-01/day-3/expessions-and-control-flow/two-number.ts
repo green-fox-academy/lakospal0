2f7e1605358bec6cf203a07700245b565ed2da39
lakospal0 <lakospal0@gmail.com>
2019-09-12 16:14:45 +0200

    I did the two-numbers thingy

---
+++ week-01/day-3/expessions-and-control-flow/two-number.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+// Print the result of 13 added to 22
+
+// Print the result of 13 substracted from 22
+
+// Print the result of 22 multiplied by 13
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+
+// Print the remainder of 22 divided by 13
+
+let numberOne,numberTwo: number;
+numberOne= 22;
+numberTwo= 13;
+
+console.log(numberOne+"+"+numberTwo+"="+(numberOne + numberTwo))
+console.log(numberOne - numberTwo)
+console.log(numberOne * numberTwo)
+console.log(numberOne / numberTwo)
+console.log(numberOne % numberTwo)
+
+
+
