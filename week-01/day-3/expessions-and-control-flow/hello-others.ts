fc0fbe4a8df6c78a3e476b4134ef6e4a6206a17d
lakospal0 <lakospal0@gmail.com>
2019-09-12 14:21:49 +0200

    I did hello-others

---
+++ week-01/day-3/expessions-and-control-flow/hello-others.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+let greetTheGuys: string;
+greetTheGuys = "Hey Chris\nHey Matt\nHey Orsi"; 
+console.log(greetTheGuys);
