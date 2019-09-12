5681592b632c1d8dc8e5f9fd8fd4ad5a500973e2
lakospal0 <lakospal0@gmail.com>
2019-09-12 17:19:46 +0200

    I did the bmi exercise

---
+++ week-01/day-3/expessions-and-control-flow/bmi.ts
@@ -0,0 +1,9 @@
+'use strict';
+//kg / magasság négyzet
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+let heightStar: number = heightInM*heightInM;
+let bmiIndex: number = massInKg/heightStar;
+console.log(bmiIndex);
+
+// Print the Body mass index (BMI) based on these values
