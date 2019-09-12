39c5bee9e3da6cd3a8e46d159f445392c249860e
lakospal0 <lakospal0@gmail.com>
2019-09-12 16:47:58 +0200

    Done but could be more good

---
+++ week-01/day-3/expessions-and-control-flow/coding-hours.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+
+let codinPerWeek:number;
+let codinPerSemester:number;
+codinPerWeek= 5*6;
+codinPerSemester=17*codinPerWeek;
+console.log(codinPerSemester)
+
+let fullSemesterHour:number;
+let avgCodinSemester:number;
+fullSemesterHour=17*52;
+avgCodinSemester=(codinPerSemester/ fullSemesterHour)*100;
+console.log(avgCodinSemester)
