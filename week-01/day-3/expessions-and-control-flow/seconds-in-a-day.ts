61027386698a35a94ad169f770464d3b97b2539b
lakospal0 <lakospal0@gmail.com>
2019-09-13 10:33:47 +0200

    I did the seconds in a day exercise

---
+++ week-01/day-3/expessions-and-control-flow/seconds-in-a-day.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+console.log (currentSeconds+ currentMinutes*60+ 14*60**2);
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by these variables
