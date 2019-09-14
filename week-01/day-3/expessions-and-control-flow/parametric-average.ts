c2d860e4e0a6e035d95558c2b6d4706894974849
lakospal0 <lakospal0@gmail.com>
2019-09-14 17:52:14 +0200

    Finished parametric-average

--- week-01/day-3/expessions-and-control-flow/parametric-average.ts
+++ week-01/day-3/expessions-and-control-flow/parametric-average.ts
@@ -3,4 +3,11 @@
 // Write a program that calculates the sum and the average from 1 to a given number.
 // Example input: 5
 // Example output: Sum: 15, Average: 3
-//???
+
+let input: number = 3;
+let sum: number = 0;
+
+for (let i=1; i<=input;i++){
+    sum+=i
+}
+console.log("SUM: "+ sum + " AVG: "+sum/input);
