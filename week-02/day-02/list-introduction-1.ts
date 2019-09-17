8d98677774eb4efc14b8fbb5bc5dfd631ef79dd5
lakospal0 <lakospal0@gmail.com>
2019-09-17 17:27:17 +0200

    list-intro-1 is half done

---
+++ week-02/day-02/list-introduction-1.ts
@@ -0,0 +1,15 @@
+//Create an empty list which will contain names (strings)
+let emptyList:string[]= [];
+//Print out the number of elements in the list
+//Add William to the list
+emptyList.push("William");
+//Print out whether the list is empty or not
+//Add John to the list
+emptyList.push("John")
+//Add Amanda to the list
+emptyList.push("Amanda")
+//Print out the number of elements in the list
+console.log(emptyList.length)
+emptyList.forEach(function(asd){console.log(asd)})
+//Print out the 3rd element
+//Iterate through the list and print out each name 
