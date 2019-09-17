9d72ad43c1cd741c1ba49f5184e2023faf8d7901
lakospal0 <lakospal0@gmail.com>
2019-09-17 15:31:46 +0200

    url-fixes adjusted

--- week-02/day-02/url-fixer.ts
+++ week-02/day-02/url-fixer.ts
@@ -2,8 +2,16 @@
 // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
 // Also, the URL is missing a crutial component, find out what it is and insert it too!
 
-let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+let thingy: string ='https//www.reddit.com/r/nevertellmethebots';
+let preThingy: string;
+let afterThingy: string;
+thingy =thingy.replace("bots","odss")
 
-url = url.replace("bots", "odds");
+function asd(thingy){
 
-console.log(url);   
+    preThingy= thingy.substring(0,5);
+    afterThingy= thingy.substring(5);
+    return preThingy+":"+afterThingy;
+}
+
+console.log(asd(thingy));
