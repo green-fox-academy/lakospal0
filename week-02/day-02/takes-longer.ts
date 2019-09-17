5888f889a0caf116f34244872af18076bbcb2e83
lakospal0 <lakospal0@gmail.com>
2019-09-17 20:27:30 +0200

    takes-longer other solution finished

--- week-02/day-02/takes-longer.ts
+++ week-02/day-02/takes-longer.ts
@@ -3,23 +3,25 @@
 // Add "always takes longer than" between the words "It" and "you"
 
 let quote: string = `Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.`;
-let preQuote:string;
-let aftrQuote:string;
+let preQuote: string;
+let aftrQuote: string;
 let place: number;
 
-
 function noobie(noobie){
-    preQuote= noobie.substring(0,20);
-    aftrQuote=noobie.substring(20);
-    return preQuote + " always takes longer than"+aftrQuote;
-
+    let place: number = noobie.indexOf('It', 0);
+    preQuote= noobie.substring(0, place + 2);
+    aftrQuote=noobie.substring(place + 2);
+    
+    return preQuote + " always takes longer than " + aftrQuote;
 }
-console.log(noobie(quote))
 
+function asd(thingy) {
+    let place: number = thingy.indexOf('It', 0);
+    let splitted = thingy.split("");
+    splitted.splice(place + 2, 0, ' always takes longer than');
 
-/*function asd(thingy) {
-    let place: number= thingy.indexOf('It', 0);
-    thingy.split("").splice(place, 0, 'always takes longer than');
-    return thingy;
-}*/
+    let result = splitted.join(''); 
+    return result;
+}
+noobie(quote);
 //console.log(asd(quote));
