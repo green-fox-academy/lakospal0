32d828f6ce70247fdf848a75afb65b44f0e8de1e
lakospal0 <lakospal0@gmail.com>
2019-09-19 10:51:38 +0200

    purple-steps task is done but not perfect

---
+++ week-02/day-03/purple-steps-ts.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r3.png]
+
+
+function square(x,y,){
+    ctx.beginPath();
+    ctx.strokeStyle="black";
+    ctx.fillStyle="purple";
+    ctx.strokeRect(x-1,y-1,22,22);
+    ctx.fillRect(x,y,20,20);
+    ctx.fill();
+    ctx.stroke();
+}
+
+
+let i:number;
+for(i=10;i<300;i+=20){
+    square(i,i);
+}
