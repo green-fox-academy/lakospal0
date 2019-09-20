5786a5307199c4fd83d5bd37d45fb9c124edd507
lakospal0 <lakospal0@gmail.com>
2019-09-20 11:27:16 +0200

    Too much lines task finished

---
+++ week-02/day-05/projectTooMuchLines.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+function vertLines(x, y, q, w) {
+    ctx.beginPath();
+    ctx.strokeStyle = "purple";
+    ctx.moveTo(x, y);
+    ctx.lineTo(q, w);
+    ctx.stroke();
+}
+
+let i: number;
+for (i = 600; i > 100; i -= 15) {
+    vertLines(i, 5, 590, i - 90)
+}
+
+function greenLines(x, y, q, w) {
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    ctx.moveTo(x, y);
+    ctx.lineTo(q, w);
+    ctx.stroke();
+}
+
+let k: number;
+for (k = 600; k > 100; k -= 15) {
+    greenLines(k - 90, 590, 5, k)
+}
