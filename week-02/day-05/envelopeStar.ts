5a57a149a20f13863225d87dc5b7e59f60435f29
lakospal0 <lakospal0@gmail.com>
2019-09-20 15:19:50 +0200

    minor adjustments in code readability

--- week-02/day-05/envelopeStar.ts
+++ week-02/day-05/envelopeStar.ts
@@ -3,8 +3,20 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
+linies(canvas.width / 2, canvas.height/2)
+
+function linies(vertX, vertY,) {
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    ctx.moveTo(canvas.width / 2, canvas.height);
+    ctx.lineTo(vertX, vertY);
+    ctx.stroke();
+
+}
 
 
+
+//BOT LEFT PART
 function botLeft(x, y, q, w) {
     ctx.beginPath();
     ctx.strokeStyle = "green";
@@ -14,65 +26,67 @@ function botLeft(x, y, q, w) {
 
 }
 
-let x: number = canvas.width / 2;
+let x: number = canvas.width / 2 + 1;
 let q: number = canvas.width / 2;
 let w: number = canvas.height / 2;
 
 for (let y = canvas.height; y >= canvas.height / 2; y -= 10) {
-    q-=10
-    botLeft(x,y,q,w);
+    q -= 10
+    botLeft(x, y, q, w);
 }
 
+//TOP LEFT PART
 function topLeft(mvtoX, mvtoY, lntoX, lntoY) {
     ctx.beginPath();
     ctx.strokeStyle = "green";
     ctx.moveTo(mvtoX, mvtoY);
-    ctx.lineTo(lntoX,lntoY);
+    ctx.lineTo(lntoX, lntoY);
     ctx.stroke();
 
 }
 
-let mvtoX:number=canvas.width/2;
-let lntoX:number=0;
-let lntoY:number= canvas.height/2;
+let mvtoX: number = canvas.width / 2;
+let lntoX: number = 0;
+let lntoY: number = canvas.height / 2;
 
-for(let mvtoY=canvas.height/2;mvtoY>=0;mvtoY-=10){
-    lntoX+=10
-    topLeft(mvtoX,mvtoY,lntoX,lntoY)
+for (let mvtoY = canvas.height / 2; mvtoY >= 0; mvtoY -= 10) {
+    lntoX += 10
+    topLeft(mvtoX, mvtoY, lntoX, lntoY)
 }
 
+//BOT RIGHT PART
 function botRight(brmvX, brmvY, brlnX, brlnY) {
     ctx.beginPath();
     ctx.strokeStyle = "green";
     ctx.moveTo(brmvX, brmvY);
-    ctx.lineTo(brlnX,brlnY);
+    ctx.lineTo(brlnX, brlnY);
     ctx.stroke();
 
 }
 
-let brmvX:number=canvas.width/2;
-let brlnX:number=canvas.width/2;
-let brlnY:number=canvas.height/2;
+let brmvX: number = canvas.width / 2;
+let brlnX: number = canvas.width / 2;
+let brlnY: number = canvas.height / 2;
 
-for(let brmvY=canvas.height;brmvY>=canvas.height/2;brmvY-=10){
-    brlnX+=10
-    botRight(brmvX,brmvY,brlnX,brlnY)
+for (let brmvY = canvas.height; brmvY >= canvas.height / 2; brmvY -= 10) {
+    brlnX += 10
+    botRight(brmvX, brmvY, brlnX, brlnY)
 }
 
-
+//TOP RIGHT PART
 function topRight(trmvX, trmvY, trlnX, trlnY) {
     ctx.beginPath();
     ctx.strokeStyle = "green";
     ctx.moveTo(trmvX, trmvY);
-    ctx.lineTo(trlnX,trlnY);
+    ctx.lineTo(trlnX, trlnY);
     ctx.stroke();
 }
 
-let trmvY:number=canvas.height/2;
-let trlnX:number=canvas.width/2;
-let trlnY:number=canvas.height/2;
+let trmvY: number = canvas.height / 2;
+let trlnX: number = canvas.width / 2;
+let trlnY: number = canvas.height / 2;
 
-for(let trmvX=canvas.width;trmvX>=canvas.width/2;trmvX-=10){
-    trlnY-=10
-    topRight(trmvX,trmvY,trlnX,trlnY)
+for (let trmvX = canvas.width; trmvX >= canvas.width / 2; trmvX -= 10) {
+    trlnY -= 10
+    topRight(trmvX, trmvY, trlnX, trlnY)
 }
