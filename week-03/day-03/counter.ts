4e73d2fb45c2b2e99745e6961b61d8084d36b44a
lakospal0 <lakospal0@gmail.com>
2019-09-25 17:05:49 +0200

    readability improvement

--- week-03/day-03/counter.ts
+++ week-03/day-03/counter.ts
@@ -1,33 +1,33 @@
 class Counter {
-    int:number;
-    startingNumber:number;
-    
-    constructor(number?:number) {
-        if(number){
-            this.int=number;
-        }else{
-            this.int=0;
+    int: number;
+    startingNumber: number;
+
+    constructor(number?: number) {
+        if (number) {
+            this.int = number;
+        } else {
+            this.int = 0;
         }
 
-        this.startingNumber=this.int;
-        
+        this.startingNumber = this.int;
+
     }
-    add(amountOf?:number){
-        if(amountOf){
-            this.int +=amountOf;
-        }else{
-            this.int ++;
+    add(amountOf?: number) {
+        if (amountOf) {
+            this.int += amountOf;
+        } else {
+            this.int++;
         }
-        
+
     }
-    getInt(){
+    getInt() {
         console.log(this.int);
     }
-    reset(){
-        this.int=this.startingNumber;
+    reset() {
+        this.int = this.startingNumber;
     }
 }
-let createdNumber= new Counter();
+let createdNumber = new Counter();
 createdNumber.add(15);
 createdNumber.add(15);
 createdNumber.reset();
