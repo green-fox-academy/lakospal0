8f4d453862406b44ee0836798dba041b1120b645
lakospal0 <lakospal0@gmail.com>
2019-09-25 14:07:58 +0200

    post-it task is done

---
+++ week-03/day-03/post-it.ts
@@ -0,0 +1,19 @@
+class PostIt {
+    backgroundColor: string;
+    text: string;
+    textColor: string;
+    constructor(backGroundColor: string, text: string, textColor: string) {
+        this.backgroundColor = backGroundColor;
+        this.text = text;
+        this.textColor = textColor;
+
+    }
+}
+
+let orange = new PostIt("orange", "Idea1", "blue");
+let pink = new PostIt("pink", "Awesome", "black");
+let yellow = new PostIt("yellow", "Superb!", "green");
+
+console.log(orange);
+
+
