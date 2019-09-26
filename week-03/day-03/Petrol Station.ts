1f2a8728466244583afafd6610728af34c1ee4fa
lakospal0 <lakospal0@gmail.com>
2019-09-26 13:56:53 +0200

    petrol station task is done

---
+++ week-03/day-03/Petrol Station.ts
@@ -0,0 +1,29 @@
+class Station {
+    gasAmount:number;
+    
+    constructor (){
+        this.gasAmount = 500;
+    }
+
+    refill(car:Car){
+        this.gasAmount --;
+        car.gasAmount ++;
+    }
+}
+
+class Car {
+    gasAmount:number;
+    capacity:number;
+    constructor(){
+        this.gasAmount=0;
+        this.capacity=100;
+    }
+
+}
+
+let shell = new Station();
+let audi = new Car();
+
+shell.refill(audi);
+console.log(shell.gasAmount);
+console.log(audi.gasAmount);
