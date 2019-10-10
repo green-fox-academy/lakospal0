f6d771df1e56dea637fc4c33371b2f0341caeb0b
lakospal0 <lakospal0@gmail.com>
2019-10-10 17:32:29 +0200

    asd

--- week-05/aircraft.ts
+++ week-05/aircraft.ts
@@ -1,9 +1,10 @@
 'use strict';
 
-class Aircraft {
+export class Aircraft {
   protected _currentAmmo: number = 0;
   protected _maxAmmo: number;
   protected _baseDmg: number;
+  protected _whatIsIt: boolean;
   constructor(maxAmmo: number = 0, baseDmg: number = 0) {
     this._maxAmmo = maxAmmo;
     this._baseDmg = baseDmg;
@@ -13,30 +14,51 @@ class Aircraft {
     let dmgDeal = this._maxAmmo * this._baseDmg;
     return dmgDeal;
   }
-  public refill(number: number) {
-    if (number <= this._maxAmmo){
+  public refill(number: number): number {
+    if (number <= this._maxAmmo) {
       this._currentAmmo += number
-      let remain:number = number - this._maxAmmo
+      let remain: number = 0;
       return remain;
-    }else {
-        this._currentAmmo = this._maxAmmo
-        let remain:number= number - this._maxAmmo;
+    } else {
+      this._currentAmmo = this._maxAmmo
+      let remain: number = number - this._maxAmmo;
       return remain;
+    }
+  }
+  public getType(): string {
+    if (this._whatIsIt) {
+      return "F35"
+    } else {
+      return "F16"
+    }
+  }
+  public getStatus():any {
+    return "Type: "+this.getType()+", Ammo: " +this._currentAmmo +", Base Dam: "+ this._baseDmg+", All dmg: "+ this._currentAmmo*this._baseDmg;
+  }
+  public isPriority(){
+    if(this._whatIsIt){
+      return true;
+    }else{
+      return false;
     }
   }
 }
 
-class F16 extends Aircraft {
+export class F16 extends Aircraft {
+  protected _whatIsIt: boolean = false;
   constructor(maxAmmo: number = 8, baseDmg: number = 30) {
     super(maxAmmo, baseDmg)
   }
 }
 
-class F35 extends Aircraft {
+export class F35 extends Aircraft {
+  protected _whatIsIt: boolean = true;
   constructor(maxAmmo: number = 12, baseDmg: number = 50) {
     super(maxAmmo, baseDmg)
   }
 }
 
 let f35 = new F35();
-console.log(f35.refill(100))
+f35.refill(5);
+f35.getStatus();
+console.log(f35.getStatus());
---
+++ week-05/carrier.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+import { Aircraft,F16,F35 }from "./aircraft"
+
+class Carrier {
+  protected _aircrafts:Aircraft[]=[];
+  
+}
