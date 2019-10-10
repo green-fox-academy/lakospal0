9b0c3845af8c302d1e92517a44ae01be362327ad
lakospal0 <lakospal0@gmail.com>
2019-10-10 20:42:05 +0200

    cheatsheet

---
+++ cheatsheet.ts
@@ -0,0 +1,36 @@
+//'use strict';
+// RANDOM NAME GENERATOR
+// 0-6 random number Math.floor(Math.random()*(7));
+// 4-10 random number Math.floor(Math.random()*(10-3)+4);
+// 1-8 random number Math.floor(Math.random()*(8)+1);
+//(large - small + 1) + small 
+//---------------------------------------------------------------------------------------------------------
+//FILE READ 
+/*
+'use strict'; 
+declare function require(path: string): any;
+export{};
+const fs = require('fs');
+
+let file:string[];
+
+try {
+  file = fs.readFileSync('file to read', 'utf-8');
+}
+catch{
+  console.log("File reading is failed for some reason")
+}
+-------------------------------------------------------------------------------------------
+WRITE
+'use strict';
+declare function require(path: string): any;
+export{};
+const fs = require('fs');
+
+try{
+    let myName : string = 'Aboud geha';
+    fs.writeFileSync('where to write',what to write,'utf8');
+} catch (err) {
+    console.log("Unable to write file: my-file.txt");
+}
+*/
