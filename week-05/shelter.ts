041d01feedc388c3e40201a6991fd8fa7e2752a7
lakospal0 <lakospal0@gmail.com>
2019-10-10 14:27:15 +0200

    improvement

--- week-05/shelter.ts
+++ week-05/shelter.ts
@@ -1,25 +1,38 @@
 'use strict';
-import { Dog,Parrot,Cat, Animal} from "./animal"
+import { Dog, Parrot, Cat, Animal } from "./animal"
 
 class Shelter {
-  protected _budget:number;
-  protected _animals:Animal[]=[];
-  protected _adoptersName:string[]=[];
-  
-  constructor(budget:number=50){
-    this._budget=budget;
+  protected _budget: number;
+  protected _animals: Animal[] = [];
+  protected _adoptersName: string[] = [];
+
+  constructor(budget: number = 50) {
+    this._budget = budget;
   }
 
-  public rescue(animal:Animal){
+  public rescue(animal: Animal) {
     this._animals.push(animal);
     return this._animals.length;
   }
-  public heal(){
-    for(let i=0;i < this._animals.length;i++){
-      if(this._animals[i].isHealthy == false){
-
+  public heal(): number {
+    for (let i = 0; i < this._animals.length; i++) {
+      if (this._animals[i].isHealthy == false && this._budget >= this._animals[i].healCost) {
+        this._animals[i].heal();
+        this._budget -= this._animals[i].healCost;
+        return 1;
       }
     }
+    return 0;
+  }
+  public addAdopter(name: string): void {
+    this._adoptersName.push(name);
+  }
+  public earnDonation(amount: number):number{
+    this._budget += amount;
+    return this._budget;
+  }
+  public toString(){
+    
   }
 }
 
