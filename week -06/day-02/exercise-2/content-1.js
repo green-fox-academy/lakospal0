b80a7548565ceb6afe85fb35e8c2fcb8bfae816d
lakospal0 <lakospal0@gmail.com>
2019-10-22 14:49:59 +0200

    second exercise is done

---
+++ week -06/day-02/exercise-2/content-1.js
@@ -0,0 +1,12 @@
+//1)  Fill every paragraph with the last one's content.
+let correctWords = document.querySelector(".animals");
+console.log(correctWords.innerText);
+let pArray = document.querySelectorAll("p");
+console.log(pArray)
+
+for (let i = 0; i < pArray.length; i++) {
+    pArray[i].innerHTML = correctWords.innerHTML;
+}
+
+console.log(pArray);
+//2)  Do the same again, but you should keep the cat strong.
