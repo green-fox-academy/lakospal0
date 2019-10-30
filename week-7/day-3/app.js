359badda4bee9f6f0cb7730161af8b18049a9102
lakospal0 <lakospal0@gmail.com>
2019-10-30 15:51:29 +0100

    some things are done

--- week-7/day-3/app.js
+++ week-7/day-3/app.js
@@ -26,6 +26,31 @@ app.get('/doubling', function(req, res) {
     res.send(input);
 })
 
+
+app.get('/greeter', function(req, res) {
+    if (req.query.name && req.query.title) {
+        var name = req.query.name;
+        var title = req.query.title;
+        var obj = {
+            "welcome_message": "Oh, hi there " + name + ", my dear " + title + "!"
+        }
+    } else if (req.query.name || req.query.title !== true) {
+        var obj = {
+            "error": "Please provide a name and a title!"
+        }
+    }
+    res.send(obj);
+})
+
+
+app.get('/appenda/:appendable', function(req, res) {
+    var appendable = req.params.appendable;
+    var append = {
+        "appended": appendable + "a"
+    }
+    res.send(append);
+})
+
 app.listen(PORT, () => {
     console.log(`The server is up and running on ${PORT}`);
 });
