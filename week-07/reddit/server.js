fc9909ec6453b7c3c4ef344cf9b9c8a85f1f6f36
lakospal0 <lakospal0@gmail.com>
2019-11-07 21:52:20 +0100

    reddit is ready

--- week-07/reddit/server.js
+++ week-07/reddit/server.js
@@ -6,6 +6,7 @@ const PORT = 8080;
 let conn = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
+    password: 'password',
     database: 'reddit',
 });
 
@@ -20,8 +21,60 @@ conn.connect(function(err) {
 
 const app = express();
 
+
+app.use(express.json())
+
 app.get('/hello', function(req, res) {
     res.send("hello :)");
+})
+
+// APP.GET 
+app.get('/posts', (req, res) => {
+    const queryString = "SELECT * FROM reddit_posts"
+    conn.query(queryString, (err, result) => {
+        res.setHeader("Content-type", "application/json");
+        res.send(JSON.stringify(result));
+        console.log('Client request: "Get API".')
+    });
+});
+
+// ADD NEW POST
+app.post('/posts', function(req, res) {
+    const queryString = `INSERT INTO reddit.reddit_posts (title, url, score) VALUES ('${req.body.title}', 'localhost:8080/posts/${req.body.title}', '0')`
+    conn.query(queryString, (err, result) => {
+        const query = `SELECT * FROM reddit.reddit_posts WHERE id=${result.insertId}`
+        conn.query(query, (err, post) => {
+            res.setHeader("Content-type", "application/json");
+            res.status(200);
+            res.send(JSON.stringify(post));
+        })
+    });
+});
+
+app.put('/posts/:id/upvote', function(req, res) {
+    const queryString = `UPDATE reddit.reddit_posts SET score = score + 1 WHERE id = ${req.params.id}`;
+    conn.query(queryString, () => {
+        const query = `SELECT * FROM reddit.reddit_posts WHERE id=${req.params.id}`
+        conn.query(query, (err, post) => {
+            res.setHeader("Content-type", "application/json");
+            res.status(200);
+            res.send(JSON.stringify(post));
+        })
+    })
+})
+
+app.put('/posts/:id/downvote', function(req, res) {
+    //const score = `SELECT score FROM reddit.reddit_posts WHERE id=${req.params.id}`;
+    //conn.query(score, (err, scoreresult) => {
+    const queryString = `UPDATE reddit.reddit_posts SET score = score - 1 WHERE id = ${req.params.id}`;
+    conn.query(queryString, () => {
+        const query = `SELECT * FROM reddit.reddit_posts WHERE id=${req.params.id}`
+        conn.query(query, (err, post) => {
+            res.setHeader("Content-type", "application/json");
+            res.status(200);
+            res.send(JSON.stringify(post));
+        })
+    })
 })
 
 
